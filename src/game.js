75f688143dc0f43fe16c1dc654b799b6cd6595e7
Niyati Desai <niyatihd@gmail.com>
2019-03-17 09:12:57 -0700

    Refactor code for Game class  to make it DRY

--- src/game.js
+++ src/game.js
@@ -20,82 +20,64 @@ class Game {
     this.activeTetrad.drawTetrad();
   }
 
-  // stackTetrad() {
-  //   for (let i = 0; i < this.currentTetrad.length; i++) {
-  //     for (let j = 0; j < this.currentTetrad.length; j++) {
-  //       if (!this.currentTetrad[i][j]) {
+
+
+
+  // collision (x, y, currentTetrad) {
+  //   for (let r = 0; r < currentTetrad.length; r++) {
+  //     for (let c = 0; c < currentTetrad.length; c++) {
+  //       // if the square is empty, we skip it
+  //       if (!currentTetrad[r][c]) {
   //         continue;
-  //       } else if (this.activeTetrad.yOffset + i < 0) {
-  //         this.gameOver = true;
-  //         break;
   //       }
-  //       if (this.currentTetrad[i][j]) {
-  //         // debugger
-  //         let idxJ = this.activeTetrad.xOffset + j;
-  //         let idxI = this.activeTetrad.yOffset + i;
-  //         this.newBoard.grid[idxI][idxJ] = "yellow";
-  //         // this.newBoard.drawBoardStack(idxI, idxJ);
+  //       // coordinates of the currentTetrad after movement
+  //       let newX = this.activeTetrad.xOffset + c + x;
+  //       let newY = this.activeTetrad.yOffset + r + y;
+
+  //       // conditions
+  //       if (newX < 0 || newX >= 10 || newY >= 20) {
+  //         return true;
+  //       }
+  //       // skip newY < 0; board[-1] will crush our game
+  //       if (newY < 0) {
+  //         continue;
+  //       }
+  //       // check if there is a locked currentTetrad already in place
+  //       if (this.newBoard.grid[newY][newX] !== "black") {
+  //         return true;
   //       }
   //     }
   //   }
+  //   return false;
   // }
 
+  collision(nextX, nextY, currentTetrad) {
+      let currPosX = this.activeTetrad.xOffset;
+      let currPosY = this.activeTetrad.yOffset;
+      let cols = this.newBoard.columns; //10
+      let rows = this.newBoard.rows; //20
 
-  collision (x, y, currentTetrad) {
-    for (let r = 0; r < currentTetrad.length; r++) {
-      for (let c = 0; c < currentTetrad.length; c++) {
-        // if the square is empty, we skip it
-        if (!currentTetrad[r][c]) {
-          continue;
-        }
-        // coordinates of the currentTetrad after movement
-        let newX = this.activeTetrad.xOffset + c + x;
-        let newY = this.activeTetrad.yOffset + r + y;
+      for (let i = 0; i < currentTetrad.length; i++) {
+        for (let j = 0; j < currentTetrad.length; j++) {
+          let nextPosX = currPosX + j + nextX;
+          let nextPosY = currPosY + i + nextY;
 
-        // conditions
-        if (newX < 0 || newX >= 10 || newY >= 20) {
-          return true;
-        }
-        // skip newY < 0; board[-1] will crush our game
-        if (newY < 0) {
-          continue;
-        }
-        // check if there is a locked currentTetrad already in place
-        if (this.newBoard.grid[newY][newX] !== "black") {
-          return true;
+          if (!currentTetrad[i][j]) { //check if tetrad cell === 0
+            continue;
+          }
+          if (nextPosY < 0) {
+            continue;
+          }
+
+          if (nextPosX >= cols || nextPosX < 0 || nextPosY >= rows) { //check walls
+            return true;
+          }
+          if (this.newBoard.grid[nextPosY][nextPosX] !== this.newBoard.baseColor) { //check adjacent cell to be empty and on board
+            return true;
+          }
         }
       }
     }
-    return false;
-  }
-
-  // collision(nextX, nextY, currentTetrad) {
-  //     let currPosX = this.activeTetrad.xOffset;
-  //     let currPosY = this.activeTetrad.yOffset;
-  //     let cols = this.newBoard.columns; //10
-  //     let rows = this.newBoard.rows; //20
-
-  //     for (let i = 0; i < currentTetrad.length; i++) {
-  //       for (let j = 0; j < currentTetrad.length; j++) {
-  //         let nextPosX = currPosX + j + nextX;
-  //         let nextPosY = currPosY + i + nextY;
-
-  //         if (!currentTetrad[i][j]) { //check if tetrad cell === 0
-  //           continue;
-  //         }
-  //         if (nextPosY < 0) {
-  //           continue;
-  //         }
-
-  //         if (nextPosX >= cols || nextPosX < 0 || nextPosY >= rows) { //check walls
-  //           return true;
-  //         }
-  //         if (this.newBoard.grid[nextPosY][nextPosX] !== this.newBoard.baseColor) { //check adjacent cell to be empty and on board
-  //           return true;
-  //         }
-  //       }
-  //     }
-  //   }
 
   rotateTetrad1() {
     let nextTetradRotation = this.tetrad[(this.currentRotation + 1) % this.tetrad.length];
@@ -123,26 +105,47 @@ class Game {
   }
 
 
-   stackTetrad() {
-     for (let r = 0; r < this.activeTetrad.currentTetrad.length; r++) {
-       for (let c = 0; c < this.activeTetrad.currentTetrad.length; c++) {
-         // we skip the vacant squares
-         if (!this.activeTetrad.currentTetrad[r][c]) {
-           continue;
-         }
-         // pieces to lock on top = game over
-         if (this.activeTetrad.yOffset + r < 0) {
-           alert("Game Over");
-           // stop request animation frame
-           gameOver = true;
-           break;
-         }
-         // we lock the piece
-        //  debugger
-         this.newBoard.grid[this.activeTetrad.yOffset + r][this.activeTetrad.xOffset + c] = "yellow";
-       }
-     }
-   }
+  //  stackTetrad() {
+  //    for (let r = 0; r < this.activeTetrad.currentTetrad.length; r++) {
+  //      for (let c = 0; c < this.activeTetrad.currentTetrad.length; c++) {
+  //        // we skip the vacant squares
+  //        if (!this.activeTetrad.currentTetrad[r][c]) {
+  //          continue;
+  //        }
+  //        // pieces to lock on top = game over
+  //        if (this.activeTetrad.yOffset + r < 0) {
+  //          alert("Game Over");
+  //          // stop request animation frame
+  //          gameOver = true;
+  //          break;
+  //        }
+  //        // we lock the piece
+  //       //  debugger
+  //        this.newBoard.grid[this.activeTetrad.yOffset + r][this.activeTetrad.xOffset + c] = "yellow";
+  //      }
+  //    }
+  //  }
+
+  stackTetrad() {
+    for (let i = 0; i < this.activeTetrad.currentTetrad.length; i++) {
+      for (let j = 0; j < this.activeTetrad.currentTetrad.length; j++) {
+        if (!this.activeTetrad.currentTetrad[i][j]) {
+          continue;
+        } else if (this.activeTetrad.yOffset + i < 0) {
+          alert("Game Over");
+          this.gameOver = true;
+          break;
+        }
+        if (this.currentTetrad[i][j]) {
+          // debugger
+          let idxJ = this.activeTetrad.xOffset + j;
+          let idxI = this.activeTetrad.yOffset + i;
+          this.newBoard.grid[idxI][idxJ] = "yellow";
+          // this.newBoard.drawBoardStack(idxI, idxJ);
+        }
+      }
+    }
+  }
  
 
 
